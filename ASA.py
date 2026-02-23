37abb23b6b6cbf4e179ca671b06f643168a98555
upnalab <landerjn@gmail.com>
2026-02-23 11:47:17 +0100

    Functioning Time Multiplexing
    t_mux can be set to create more amp and phase arrays for time multiplexing.
    Really slow right now, have to optimize.
    
    TODO:
     - Calculate all t_mux in a single pass instead of loop
     - General optimizations required (have to check where)

--- ASA.py
+++ ASA.py
@@ -35,32 +35,43 @@ class SimulationASA:
     # fr: Frequency of emitters
     # c: speed of sound
 
-    def __init__(self, fr, c, size=0.16, pos: list[float] = [0, 0, 0.09], ds=0.16 / 64):
+    def __init__(
+        self,
+        fr,
+        c,
+        size=0.16,
+        pos: list[float] = [0, 0, 0.09],
+        ds=0.16 / 64,
+        device="cpu",
+    ):
         # Common data for all fields
         self.fr = fr
         self.c = c
         self.wavelen = c / fr
         self.k = 2 * math.pi / self.wavelen
-        self.pos = torch.tensor(pos, dtype=torch.float32)
+        self.pos = torch.tensor(pos, dtype=torch.float32, device=device)
         self.ds = ds
         self.size = size
         self.dim = int(self.size / self.ds)
+        self.device = device
 
         self.transducers: list[
             Transducer
         ] = []  # Array of num_emitter x num_emitter phases
-        self.propagator: list[torch.tensor] = []  # Propagator matrix
+        self.propagator: list[torch.Tensor] = []  # Propagator matrix
 
     def add_transducer(self, transducer):
         self.transducers.append(transducer)
 
-    def emmiter_field_from_phases(self, emitter_dim: int, phases: np.ndarray):
+    def emmiter_field_from_phases(self, emitter_dim: int, phases: torch.Tensor):
         assert phases.numel() == emitter_dim * emitter_dim, (
             "Not enough phases for all emitters"
         )
 
         cell_size = self.size / self.dim
-        transducer = torch.zeros((self.dim, self.dim), dtype=torch.complex64)
+        transducer = torch.zeros(
+            (self.dim, self.dim), dtype=torch.complex64, device=self.device
+        )
 
         cells_per_emitter = np.round(self.emitter_size / cell_size).astype(np.int32)
         gap_between = (self.dim - cells_per_emitter * emitter_dim) // emitter_dim
@@ -79,22 +90,30 @@ class SimulationASA:
     def create_propagators(self):
         for transducer in self.transducers:
             propagator = torch.zeros(
-                (self.dim, self.dim, self.dim), dtype=torch.complex64
+                (self.dim, self.dim, self.dim),
+                dtype=torch.complex64,
+                device=self.device,
             )
 
             # Create the coordinate grid
             z_coords = (
-                torch.arange(-self.dim // 2, self.dim // 2).view(1, 1, -1).float()
+                torch.arange(-self.dim // 2, self.dim // 2, device=self.device)
+                .view(1, 1, -1)
+                .float()
                 * self.ds
                 + self.pos[0]
             )
             y_coords = (
-                torch.arange(-self.dim // 2, self.dim // 2).view(1, -1, 1).float()
+                torch.arange(-self.dim // 2, self.dim // 2, device=self.device)
+                .view(1, -1, 1)
+                .float()
                 * self.ds
                 + self.pos[1]
             )
             x_coords = (
-                torch.arange(-self.dim // 2, self.dim // 2).view(-1, 1, 1).float()
+                torch.arange(-self.dim // 2, self.dim // 2, device=self.device)
+                .view(-1, 1, 1)
+                .float()
                 * self.ds
                 + self.pos[2]
             )
@@ -125,53 +144,67 @@ class SimulationASA:
 
     def calculate_volume(self):
         # For now assume there is only one for testing :P
-        volume = torch.zeros((self.dim, self.dim, self.dim), dtype=torch.complex64)
+        volume = torch.zeros(
+            (self.dim, self.dim, self.dim), dtype=torch.complex64, device=self.device
+        )
 
+        total_mux = 0
         for transducer in self.transducers:
-            emitter = transducer.to_complex_plane(self.ds)
-            propagator = self.propagator[0]
+            total_mux += transducer.t_mux
 
-            min_side_size = propagator.shape[-1] + emitter.shape[-1] - 1
-            pad_to_size = 2 ** np.ceil(np.log2(min_side_size))
+            for t in range(transducer.t_mux):
+                emitter = transducer.to_complex_plane(self.ds, t)
 
-            pad_func = torch.nn.functional.pad
+                # plt.imshow(torch.real(emitter).detach().numpy())
+                # plt.show(block=True)
 
-            prop_pad = int(np.ceil((pad_to_size - propagator.shape[-1]) / 2))
-            padded_propagator = pad_func(
-                propagator, (prop_pad, prop_pad, prop_pad, prop_pad, 0, 0)
-            )
+                propagator = self.propagator[0]
 
-            emitt_pad = int(np.ceil((pad_to_size - emitter.shape[-1]) / 2))
-            padded_emitter = pad_func(
-                emitter, (emitt_pad, emitt_pad, emitt_pad, emitt_pad)
-            )
+                min_side_size = (
+                    propagator.shape[-1]
+                    + transducer.to_complex_plane(self.ds, 0).shape[-1]
+                    - 1
+                )
+                pad_to_size = 2 ** np.ceil(np.log2(min_side_size))
 
-            fft_emitter = torch.fft.fft2(torch.fft.ifftshift(padded_emitter))
-            fft_prop = torch.fft.fft2(torch.fft.ifftshift(padded_propagator))
+                pad_func = torch.nn.functional.pad
 
-            convolved = fft_emitter * fft_prop
+                prop_pad = int(np.ceil((pad_to_size - propagator.shape[-1]) / 2))
+                padded_propagator = pad_func(
+                    propagator, (prop_pad, prop_pad, prop_pad, prop_pad, 0, 0)
+                )
 
-            field = torch.fft.fftshift(torch.fft.ifft2(convolved))
+                emitt_pad = int(np.ceil((pad_to_size - emitter.shape[-1]) / 2))
+                padded_emitter = pad_func(
+                    emitter, (emitt_pad, emitt_pad, emitt_pad, emitt_pad)
+                )
 
-            # Trim the field
-            extra = field.size()[1] - self.dim
-            field = field[
-                :,
-                extra // 2 : extra // 2 + self.dim,
-                extra // 2 : extra // 2 + self.dim,
-            ]
+                fft_emitter = torch.fft.fft2(torch.fft.ifftshift(padded_emitter))
+                fft_prop = torch.fft.fft2(torch.fft.ifftshift(padded_propagator))
 
-            if transducer.orientation == Orientation.Z:
-                volume += field
-            elif transducer.orientation == Orientation.Z_1:
-                volume += field.rot90(2, (0, 1))
-            elif transducer.orientation == Orientation.Y:
-                volume += field.rot90(1, (0, 1))
-            elif transducer.orientation == Orientation.X:
-                volume += field.rot90(1, (0, 2))
+                convolved = fft_emitter * fft_prop
+
+                field = torch.fft.fftshift(torch.fft.ifft2(convolved))
+
+                # Trim the field
+                extra = field.size()[1] - self.dim
+                field = field[
+                    :,
+                    extra // 2 : extra // 2 + self.dim,
+                    extra // 2 : extra // 2 + self.dim,
+                ]
+
+                if transducer.orientation == Orientation.Z:
+                    volume += field
+                elif transducer.orientation == Orientation.Z_1:
+                    volume += field.rot90(2, (0, 1))
+                elif transducer.orientation == Orientation.Y:
+                    volume += field.rot90(-1, (0, 1))
+                elif transducer.orientation == Orientation.X:
+                    volume += field.rot90(-1, (0, 2))
 
         # print("FFTs Finished!")
-        return volume
+        return volume.abs() / total_mux
 
 
 class Orientation(Enum):
@@ -190,43 +223,64 @@ class Transducer:
         emitter_size: float,
         pos: list,
         orientation: Orientation = Orientation.Z,
+        t_mux: int = 1,
+        device="cpu",
     ):
-        self.phases: torch.tensor
-        self.amps: torch.tensor
+        self.phases: list[torch.Tensor]
+        self.amps: list[torch.Tensor]
         self.emitter_size: float = emitter_size
         self.array_size: float = array_size
-        self.pos: list = torch.tensor(pos, dtype=torch.float32)
+        self.pos: torch.Tensor = torch.tensor(pos, dtype=torch.float32, device=device)
         self.orientation: Orientation = orientation
+        self.t_mux: int = t_mux
+        self.device = device
 
-        self.phases = torch.zeros(
-            (emitters_num, emitters_num), dtype=torch.float32, requires_grad=True
-        )
-        self.amps = torch.ones(
-            (emitters_num, emitters_num), dtype=torch.float32, requires_grad=True
-        )
+        self.phases = [
+            torch.rand(
+                (emitters_num, emitters_num),
+                dtype=torch.float32,
+                requires_grad=True,
+                device=self.device,
+            )
+            for i in range(t_mux)
+        ]
+        self.amps = [
+            torch.rand(
+                (emitters_num, emitters_num),
+                dtype=torch.float32,
+                requires_grad=True,
+                device=self.device,
+            )
+            for i in range(t_mux)
+        ]
 
-    def to_complex_plane(self, ds: float):
+    def to_complex_plane(self, ds: float, t_mux: int):
+        start = time.time()
         # Input:
         # - ds: Sice of each cell. Defined by the simulation volume
         target_size = round(self.array_size / ds)
         # print(target_size)
 
-        transducer = torch.zeros((target_size, target_size), dtype=torch.complex64)
+        transducer = torch.zeros(
+            (target_size, target_size), dtype=torch.complex64, device=self.device
+        )
 
         cells_per_emitter = np.round(self.emitter_size / ds).astype(np.int32)
         gap_between = (
-            target_size - cells_per_emitter * self.phases.size()[0]
-        ) // self.phases.size()[0]
+            target_size - cells_per_emitter * self.phases[t_mux].size()[0]
+        ) // self.phases[t_mux].size()[0]
 
         # print(cells_per_emitter, gap_between)
 
-        for y in range(self.phases.size()[0]):
-            for x in range(self.phases.size()[0]):
+        for y in range(self.phases[t_mux].size()[0]):
+            for x in range(self.phases[t_mux].size()[0]):
                 pos_x = int(gap_between // 2 + x * (cells_per_emitter + gap_between))
                 pos_y = int(gap_between // 2 + y * (cells_per_emitter + gap_between))
 
                 transducer[
                     pos_y : pos_y + cells_per_emitter, pos_x : pos_x + cells_per_emitter
-                ] += self.amps[y, x] * torch.exp(1j * self.phases[y, x])
+                ] += self.amps[t_mux][y, x] * torch.exp(1j * self.phases[t_mux][y, x])
+
+        # print(f"time took: {time.time() - start}")
 
         return transducer
--- test_asa.py
+++ test_asa.py
@@ -9,18 +9,34 @@ import torch
 
 from ASA import Orientation, SimulationASA, Transducer
 
+torch.manual_seed(68)
+
 fr = 40e3
 c = 343.0
 
-sim = SimulationASA(fr, c, size=0.08, ds=0.08 / 64)
-sim.add_transducer(Transducer(16, 0.16, 0.005, [0, 0, 0], Orientation.Z))
-sim.add_transducer(Transducer(16, 0.16, 0.005, [0, 0, 0], Orientation.X))
-sim.add_transducer(Transducer(16, 0.16, 0.005, [0, 0, 0], Orientation.Y))
+device = "cuda"
+
+sim = SimulationASA(fr, c, size=0.16, ds=0.16 / 25, device=device)
+
+# sim.add_transducer(
+#     Transducer(16, 0.16, 0.008, [0, 0, 0], Orientation.X, t_mux=1, device=device)
+# )
+
+sim.add_transducer(
+    Transducer(16, 0.16, 0.008, [-0.12, 0, 0], Orientation.X, t_mux=16, device=device)
+)
+sim.add_transducer(
+    Transducer(16, 0.16, 0.008, [-0.12, 0, 0], Orientation.Y, t_mux=16, device=device)
+)
+sim.add_transducer(
+    Transducer(16, 0.16, 0.008, [-0.12, 0, 0], Orientation.Z, t_mux=16, device=device)
+)
+
 sim.create_propagators()
 
 
-size = 64
-sigma = size / 24  # controls spread (smaller = sharper sphere)
+size = 25
+sigma = size / 12  # controls spread (smaller = sharper sphere)
 
 # Create coordinate grid
 coords = torch.linspace(-1, 1, size)
@@ -30,28 +46,50 @@ x, y, z = torch.meshgrid(coords, coords, coords, indexing="ij")
 r2 = x**2 + y**2 + z**2
 
 # Gaussian
-sample = torch.exp(-r2 / (2 * (sigma / size) ** 2)) * 10
+sample = torch.exp(-r2 / (2 * (sigma / size) ** 2)).to(device) * 2
 
-print(sample.mean())
+# print(sample.max())
 
-field = None
-for k in range(20):
+
+params = []
+
+for tr in sim.transducers:
+    for t in range(tr.t_mux):
+        params.append(tr.phases[t])
+        params.append(tr.amps[t])
+
+optimizer = torch.optim.AdamW(params, lr=0.1)
+for k in range(300):
     field = sim.calculate_volume()
+    field = field.abs()  # / torch.abs(field).max()
 
-    loss = (field.abs() - sample).abs().sum()
+    loss = ((field - sample) ** 2).sum()
     loss.backward()
 
-    print(loss.item())
+    optimizer.step()
+    optimizer.zero_grad()
 
-    with torch.no_grad():
-        for tr in sim.transducers:
-            tr.phases -= 0.001 * tr.phases.grad
-            tr.amps -= 0.001 * tr.amps.grad
+    print(f"{loss.item():.2f}")
 
-    for tr in sim.transducers:
-        tr.phases.grad = None
-        tr.amps.grad = None
+    # with torch.no_grad():
+    #     for tr in sim.transducers:
+    #         for t in range(tr.t_mux):
+    #             tr.phases[t] -= 0.00000001 * tr.phases[t].grad
+    #             tr.amps[t] -= 0.00000001 * tr.amps[t].grad
+
+    # for tr in sim.transducers:
+    #     for t in range(tr.t_mux):
+    #         tr.phases[t].grad = None
+    #         tr.amps[t].grad = None
 
 field = sim.calculate_volume()
-viewer, layers = napari.imshow(torch.abs(field).rot90(-1, (0, 1)).detach().numpy())
+
+print(torch.abs(field).mean())
+
+field_norm = field.abs() / torch.abs(field).max()
+loss = (field_norm - sample).abs()
+
+viewer, layers = napari.imshow(
+    torch.abs(field).rot90(-1, (0, 1)).cpu().detach().numpy()
+)
 napari.run()
